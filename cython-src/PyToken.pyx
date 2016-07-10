a9e61a4bf7fa33ed2a3a5728b9183621533af8d5
Joe Khoury <joe.el.k97@gmail.com>
2016-07-10 19:19:53 +0400

    Created functions to convert to and from a PyToken and a Token.

--- cython-src/PyToken.pyx
+++ cython-src/PyToken.pyx
@@ -12,5 +12,18 @@ cdef class PyToken:
     def __dealloc__(self):
         del self.c_token
 
+    @staticmethod
+    cdef PyToken PyToken_from_Token(self, const Token& convert_from):
+        """
+        Creates a PyToken object from a token object.
+        """
+        cdef string token_str = convert_from.get_token_str()
+        return PyToken(token_str)
+
+    @staticmethod
+    cdef Token Token_from_PyToken(self, convert_from):
+        assert type(convert_from) is PyToken, "make sure the second argument is of type PyToken"
+        return Token(convert_from.get_str())
+
     def get_str(self):
         return self.c_token.get_token_str()
