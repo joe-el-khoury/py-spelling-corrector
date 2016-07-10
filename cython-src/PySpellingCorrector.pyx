d65ba59f214f8f24c9ed3ce5a1d53bc8effebdfe
Joe Khoury <joe.el.k97@gmail.com>
2016-07-11 00:40:35 +0400

    Implemented the training functionality.

--- cython-src/PySpellingCorrector.pyx
+++ cython-src/PySpellingCorrector.pyx
@@ -1,6 +1,17 @@
 # distutils: language = c++
 # distutils: sources = spelling-corrector/src/SpellingCorrector.cpp
 
-cdef extern from "../spelling-corrector/src/SpellingCorrector.h":
-    cdef cppclass SpellingCorrector:
-        pass
+cimport PySpellingCorrector
+from types import * # to check for types
+
+cdef class PySpellingCorrector:
+    cdef SpellingCorrector* c_spc
+    def __init__(self):
+        self.c_spc = new SpellingCorrector()
+
+    def __dealloc__(self):
+        del self.c_spc
+
+    def train(self, file_name):
+        assert type(file_name) is StringType, "file_name should be a string"
+        self.c_spc.train(file_name)
